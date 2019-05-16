31f4bcc52c460bcd3e1482a3c2eac948c93c1ce2
danfyfe <fyfedaniel@gmail.com>
2019-05-16 14:10:46 -0400

    BUTTONS

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -23,21 +23,75 @@ fetch(TRAINERS_URL, {method: "GET"})
         </div>`;
 
       trainerContainer.appendChild(trainerDiv);
+      // add button
+      const addButton = grab(`[data-trainer-id="${trainer.id}"]`)
+        addButton.addEventListener("click",function(e){
+          fetch(POKEMONS_URL, {
+          method:"POST",
+          headers:
+          {
+            'Content-Type': 'application/json'
+          },
+          body:
+          JSON.stringify({
+            "trainer_id": trainer.id
+          })
 
+          })
+          .then(function(resp){
+            return resp.json();
+          })
+          .then(function(addPoke){
+            if (!addPoke.error) {
+              // const pokemonList = grab(`#trainer-${trainer.id}-pokemon`);
+              // debugger
+              const addNewPoke = document.createElement("li")
+              pokemonList.appendChild(addNewPoke)
+              addNewPoke.innerHTML = `${addPoke.nickname} (${addPoke.species})
+                  <button class="release" data-pokemon-id="${addPoke.id}">Release</button>`;
+            }
+
+
+
+          })
+
+
+        })
+
+
+
+      // list of each trainers pokemon
       const pokemonList = grab(`#trainer-${trainer.id}-pokemon`);
 
+      //
       trainer.pokemons.forEach(function (poke) {
         const newPoke = document.createElement("li");
-        
+
         newPoke.innerHTML = `${poke.nickname} (${poke.species})
             <button class="release" data-pokemon-id="${poke.id}">Release</button>`;
 
+
+
         pokemonList.appendChild(newPoke);
+
+          const releaseButton = grab(`[data-pokemon-id="${poke.id}"]`)
+          releaseButton.addEventListener("click", function(e){
+
+            newPoke.remove();
+            fetch(POKEMONS_URL + "/" + poke.id, {method: "DELETE"})
+            .then(function(resp){
+              return resp.json()
+            })
+
+          })
+
+
       });
 
       // debugger;
     }
   });
+
 
 
 // <div class="card" data-id="1"><p>Prince</p>
